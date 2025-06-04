0ebfeec4cc0e11224ea6cf1b8cdba115b1d45a3d
Fiidhanz <153182158+Fiidhanz@users.noreply.github.com>
2025-06-04 23:55:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import urllib.parse
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 app.secret_key = 'secretkey123'  # Untuk flash message
